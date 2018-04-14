c8e1503080b1a088da22835489709fd1c54a99bf
shaoyuncen <496064946@qq.com>
2018-04-14 21:00:22 +0800

    、
    、

--- Leetcode/190ReverseBits.cpp
+++ Leetcode/190ReverseBits.cpp
@@ -4,7 +4,7 @@ uint32_t reverseBits(uint32_t n)
 {
     uint32_t result = 0;
     for (int i = 0; i < 32; i++)
-        result = (result << 1) + (n >> i & 1);
+        result = (result << 1) + (n >> i & 1);//n>>i&1的意思是只保留末位的二进制数1 / 0
     return result;
 }
 int main()
