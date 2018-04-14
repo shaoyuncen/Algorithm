97c12261cd6268c475c425bec14aff3b082a261d
shaoyuncen <496064946@qq.com>
2018-04-14 20:47:43 +0800

    .

---
+++ Leetcode/190ReverseBits.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+uint32_t reverseBits(uint32_t n)
+{
+    uint32_t result = 0;
+    for (int i = 0; i < 32; i++)
+        result = (result << 1) + (n >> i & 1);
+    return result;
+}
+int main()
+{
+    uint32_t n = 43261596; // 00000010100101000001111010011100
+    uint32_t number = reverseBits(n); //00111001011110000010100101000000  964176192
+    cout << number << endl;
+
+    return 0;
+}
---
+++ Leetcode/7reverseInt.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int reverse(int x)
+{
+    if (x == INT_MIN)
+        return 0;
+    if (x < 0)
+        return -reverse(-x);
+
+    int rx = 0; // store reversed integer
+    while (x != 0)
+    {
+        // check overflow
+        if (rx > INT_MAX / 10)
+            return 0;
+        rx = rx * 10 + x % 10;
+        x = x / 10;
+    }
+    return rx;
+}
+int main()
+{
+    int x = 123;
+    int number = reverse(x);
+    cout << number << endl;
+    return 0;
+}
