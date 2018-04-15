7a8969f9e31c0236ae3193c6a4d620503cdf1a69
shaoyuncen <496064946@qq.com>
2018-04-15 17:59:35 +0800

    .
    .

---
+++ Leetcode/learnCpp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 42;
+    int *pi = &i;
+    cout << *pi << endl;
+    *pi = *pi * *pi;
+    cout << *pi << ' ' << i << endl;
+
+    return 0;
+}
