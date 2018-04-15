753cb6cb3db2cc3ac162df63112d97ddc2ef2858
shaoyuncen <496064946@qq.com>
2018-04-15 18:30:55 +0800

    .
    .

--- Leetcode/41_FirstMissingPositive.cpp
+++ Leetcode/41_FirstMissingPositive.cpp
@@ -18,18 +18,16 @@ using namespace std;
 //             min = nums[i];
 //         map[nums[i]] = i;
 //     }
-//     if (min == -1)
+//     if (min == -1 || min > 1)
 //         return 1;
 //     int missing = min + 1;
-//     if (min > 1)
-//         return 1;
+
 //     while (map.find(missing) != map.end())
 //     {
 //         missing++;
 //     }
 //     return missing;
 // }
-//下面的代码又臭又长，但是执行是对的，所以不想改了
 int firstMissingPositive(vector<int> &nums) //O(n) time O(1) space
 {
     int size = nums.size();
