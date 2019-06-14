e96e2cbca0396ebdbfd7e44cae8658f79c2a8447
shaoyuncen <496064946@qq.com>
2019-06-14 18:09:46 +0800

    .

---
+++ examPrac/list/reverseLinkList.cpp
@@ -0,0 +1,31 @@
+//将比x小的放List左边，比x大的放List右边
+#include <iostream>
+using namespace std;
+struct ListNode{
+    ListNode(int val):val(val){}
+    int val;
+    ListNode* next;
+};
+
+ListNode* reverseList(ListNode* head)
+{//迭代
+    ListNode* prev = NULL;
+    while(!head)
+    {
+        ListNode* curr = head;
+        head = head->next;
+        curr->next = prev;
+        prev = curr;
+    }
+    return prev;
+}  
+
+ListNode* reverseList2(ListNode* head)
+{
+    if(!head) return head;
+    if(!head->next) return head;
+    ListNode* newHead = reverseList2(head->next);
+    head->next->next = head;
+    head->next = NULL;
+    return newHead;
+}
---
+++ examPrac/list/swapNodes.cpp
@@ -0,0 +1,26 @@
+//先交换两个prev节点的next指针的值
+//再交换这个两个节点的next指针的值
+LinkNode* swapPairs(ListNode* head)
+{
+    if(!head)
+        return head;
+    
+    ListNode* dummy = new ListNode(0);
+    dummy->next = head;
+    ListNode* prev = dummy;
+    ListNode* node1 = head, *node2 = head->next;
+    while(node&&node->next)
+    {
+        node2 = node1->next;
+        //swap the "next" of prev nodes
+        prev->next = node1->next;//=node2
+
+        //swap the "next" of curr nodes
+        node1->next = node2->next;
+        node2->next = node1;
+        
+        prev = node1;
+        node1 = prev->next;
+    }
+    return dummy->next;
+}
