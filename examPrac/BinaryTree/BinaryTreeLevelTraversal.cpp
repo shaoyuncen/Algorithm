00ab53b7a16933b5ec1f139b804ee54f3152f520
shaoyuncen <496064946@qq.com>
2019-06-17 19:57:22 +0800

    Update BinaryTreeLevelTraversal.cpp

--- examPrac/BinaryTree/BinaryTreeLevelTraversal.cpp
+++ examPrac/BinaryTree/BinaryTreeLevelTraversal.cpp
@@ -1,35 +1,40 @@
 #include "tree.h"
 #include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
 //一般用两个队列或两个栈来解决
 //3,9,20,null,null,15,7的树    输出  {[3],[20,9], [7,15]}
-vector<vector<int>> LevelTraversal(TreeNode* root)
+vector<vector<int>> LevelTraversal(TreeNode *root)
 {
     vector<vector<int>> result;
 
-    bool leftToRight=true;
-    stack<TreeNode*> currLevel, nextLevel;
+    bool leftToRight = true;
+    stack<TreeNode *> currLevel, nextLevel;
     currLevel.push(root);
     vector<int> levelVec;
-    while(!currLevel.empty()){
-        TreeNode* currNode = currLevel.top();;
+    while (!currLevel.empty())
+    {
+        TreeNode *currNode = currLevel.top();
+        ;
         currLevel.pop();
-        
-        if(currNode)
-        {//当前节点不为空
+
+        if (currNode)
+        { //当前节点不为空
             // cout<<currNode->val;
             levelVec.push_back(currNode->val);
-            if(leftToRight)
+            if (leftToRight)
             {
-                currLevel.push(currNode->left);
-                currLevel.push(currNode->right);
+                nextLevel.push(currNode->left);
+                nextLevel.push(currNode->right);
             }
             else
             {
-                currLevel.push(currNode->right);
-                currLevel.push(currNode->left);
+                nextLevel.push(currNode->right);
+                nextLevel.push(currNode->left);
             }
-        }   
-        if(currLevel.empty())
+        }
+        if (currLevel.empty())
         {
             // cout<<endl;
             swap(currLevel, nextLevel);
@@ -38,5 +43,32 @@ vector<vector<int>> LevelTraversal(TreeNode* root)
             levelVec.clear();
         }
     }
-
+}
+////3,9,20,null,null,15,7的树    输出  {[3],[9,20], [15,7]}
+vector<vector<int>> levelOrder(TreeNode *root)
+{
+    vector<vector<int>> result;
+    queue<TreeNode *> currLevel, nextLevel;
+    currLevel.push(root);
+    vector<int> levelVec;
+    while (!currLevel.empty())
+    {
+        TreeNode *currNode = currLevel.front();
+        currLevel.pop();
+        if (currNode)
+        {
+            levelVec.push_back(currNode->val);
+            nextLevel.push(currNode->left);
+            nextLevel.push(currNode->right);
+        }
+        if (currLevel.empty())
+        {
+            swap(currLevel, nextLevel);
+            if (levelVec.size() == 0)
+                return result;
+            result.push_back(levelVec);
+            levelVec.clear();
+        }
+    }
+    return result;
 }
